e29224d8f38e14e3ea85f5cca29018bb10f16cb0
kshitij <71221895+kshitiz603@users.noreply.github.com>
2023-11-02 13:37:40 +0530

    Update scr_1.py

--- scr_1.py
+++ scr_1.py
@@ -6,11 +6,7 @@ import time
 import random
 import ast
 from txt import txt
-import psutil
 
-
-# Start monitoring network activity
-net_stats = psutil.net_io_counters(pernic=True)
 
 
 
@@ -29,7 +25,7 @@ driver = webdriver.Edge()
 driver.get("https://bing.com/search?q=hlo")
 
 # Load the stored cookies
-for x in range(6,8):
+for x in range(1,8):
     with open('cookies/' + str(x)+ '.txt', 'r') as cookies_file:
         cookies = ast.literal_eval(cookies_file.read())
 
@@ -45,8 +41,8 @@ for x in range(6,8):
 #search start
     driver.get("https://bing.com/search?q=hlo")
 
-    for i in range(0,4):
-        time.sleep(0.15)
+    for i in range(0,35):
+        time.sleep(0.25)
         driver.find_element(By.XPATH,
                         "/html/body/header/form/div/textarea").clear()
 
@@ -58,7 +54,7 @@ for x in range(6,8):
         driver.find_element(By.XPATH,
                         "/html/body/header/form/div/textarea").send_keys(Keys.RETURN)
         
-        time.sleep(0.2)
+        time.sleep(0.25)
         driver.find_element(By.XPATH,
                         "/html/body/header/form/div/textarea").clear()
 
@@ -78,24 +74,7 @@ for x in range(6,8):
     driver.delete_all_cookies()
     time.sleep(1)
     
-
 #loop unit enter
 input("Press Enter to quit...")
 # Close the WebDriver when you're done
 driver.quit()
-
-
-
-# End monitoring
-new_net_stats = psutil.net_io_counters(pernic=True)
-
-# Calculate data usage
-bytes_sent = new_net_stats['wlp2s0'].bytes_sent - net_stats['wlp2s0'].bytes_sent
-bytes_recv = new_net_stats['wlp2s0'].bytes_recv - net_stats['wlp2s0'].bytes_recv
-
-
-megabytes_sent = bytes_sent / 1024 / 1024
-megabytes_recv = bytes_recv / 1024 / 1024
-
-print(f"Data sent: {megabytes_sent:.2f} MB")
-print(f"Data received: {megabytes_recv:.2f} MB")
